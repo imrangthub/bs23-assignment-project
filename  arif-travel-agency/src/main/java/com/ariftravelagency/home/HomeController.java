48297ec5c987fac5f6311c1a07b2afd98f095dc3
MD IMRAN HOSSAIN <imranmadbar@gmail.com>
2021-07-02 22:12:24 +0600

    add UI Related dependency (thymeleaf and bootstrap 4)

---  arif-travel-agency/src/main/java/com/ariftravelagency/home/HomeController.java
+++  arif-travel-agency/src/main/java/com/ariftravelagency/home/HomeController.java
@@ -1,14 +1,14 @@
 package com.ariftravelagency.home;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class HomeController {
 
 	@GetMapping("/")
 	public String index() {
-		return "Greeting from home Controller";
+		return "home";
 	}
 
 }
